5f9b24af8318b270a11d99ab1cc5802581e1d5bf
parkjunsun <qkrwnstns52@naver.com>
2021-03-15 20:35:21 +0900

    commit

---
+++ src/main/java/hello/servlet/basic/request/RequestParmaServlet.java
@@ -0,0 +1,45 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+
+
+@WebServlet(name = "requestParmaServlet", urlPatterns = "/request-param")
+public class RequestParmaServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("[전체 파라미터 조회] - start");
+
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+
+        System.out.println("[전체 파라미터 조회] - finish");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println("[단일 파라미터 조회] - finish");
+        System.out.println();
+
+        System.out.println("[이름이 같은 복수 파라미터 조회] - start");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("name = " + name);
+        }
+        System.out.println("[이름이 같은 복수 파라미터 조회] - finish");
+        response.getWriter().write("ok");
+    }
+}
